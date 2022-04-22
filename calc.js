5844eb7969254f0fa9ce91f4fbc466273ae3e5c5
Lene Signorelli <103646528+lenesignorelli-warren@users.noreply.github.com>
2022-04-22 14:23:03 -0300

    first commit

---
+++ calc.js
@@ -0,0 +1,8 @@
+var SomaFunc = require("./soma");
+var SubFunc = require("./sub");
+var MultFunc = require("./mult");
+
+
+console.log(SomaFunc(1,2));
+console.log(SubFunc(10,5));
+console.log(MultFunc(2,5));
